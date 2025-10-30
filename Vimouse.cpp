7b271fbb4511eb3a3e11f2fdf0cfa4c7ec105802
Ethan Jiang <jyzgo0125@gmail.com>
2025-10-30 16:05:49 +0800

    when back to focus , will move cursot to center

--- Vimouse.cpp
+++ Vimouse.cpp
@@ -1091,6 +1091,14 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
         if (isKeyDown && vkCode == 'J' && g_ctrlPressed) {
             g_isActive = !g_isActive;
             if (g_isActive) {
+
+                g_currentScreenIndex = GetCurrentScreenIndex();
+                const RECT& screenRect = g_screenRects[g_currentScreenIndex];
+                int centerX = screenRect.left + (screenRect.right - screenRect.left) / 2;
+                int centerY = screenRect.top + (screenRect.bottom - screenRect.top) / 2;
+
+                SetCursorPos(centerX , centerY);
+
                 GetCursorPos(&g_lastMousePos);  // 记录当前位置
                 // 重置C键状态
                 g_firstCPress = true;
